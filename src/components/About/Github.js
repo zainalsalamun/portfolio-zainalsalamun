fd6f704033d2afbdd63c47548707f5090281e59c
binar-rizki <zainalrtf@gmail.com>
2024-05-20 15:19:37 +0700

    change github

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -8,13 +8,23 @@ function Github() {
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-      <GitHubCalendar
-        username="zainalsalamun"
-        blockSize={15}
-        blockMargin={5}
-        color="#5564f5"
-        fontSize={16}
-      />
+      <a
+        href="https://github.com/zainalsalamun"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none" }}
+      >
+        <GitHubCalendar
+          username="zainalsalamun"
+          blockSize={15}
+          blockMargin={5}
+          color="#5564f5"
+          fontSize={16}
+        />
+        <p style={{ textAlign: "center", fontSize: "1.2em" }}>
+          View my github profile <i className="fa fa-arrow-right" />
+        </p>
+      </a>
     </Row>
   );
 }
